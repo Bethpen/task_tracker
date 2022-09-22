4787d5996bb9f1e30462813380b0d31490261eea
Bethpen <jesutofunmiosunlana@gmail.com>
2022-09-22 18:54:57 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <h1>{{title}}</h1>
+    </div>
+    <Button text="Add task" color="green"/>
+</template>
+
+<script>
+    import Button from './Button.vue';
+
+    export default{
+        name: 'header',
+        components: {
+            Button
+        },
+        props: {
+            title: String
+        }
+    }
+</script>
